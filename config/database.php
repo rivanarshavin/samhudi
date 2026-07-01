c3be69f3647ed56e4807aff71686bc1b3ea05261
unknown <alifmuzakki1110@gmail.com>
2026-07-01 14:57:45 +0700

    third commit | fix struktur folder

--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$conn = mysqli_connect("localhost", "root", "", "my_database");
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
-}
