0560b4e20c5a086f65c69b496aa8c12fed5d2766
Mosesauliya <mosesauliya@gmail.com>
2026-07-15 08:38:34 +0700

    cobain

--- application/views/silsilah/familytree_view.php
+++ application/views/silsilah/familytree_view.php
@@ -18,7 +18,9 @@
                 <i class="bi bi-search"></i>
                 <input type="text" placeholder="Cari anggota...">
             </div>
-            <a href="<?php echo site_url('familytree/add'); ?>" class="btn-tambah-anggota" style="text-decoration:none;">+ Tambah Anggota</a>
+            <?php if ($this->session->userdata('logged_in')): ?>
+                <a href="<?php echo site_url('familytree/add'); ?>" class="btn-tambah-anggota" style="text-decoration:none;">+ Tambah Anggota</a>
+            <?php endif; ?>
         </div>
     </div>
 
